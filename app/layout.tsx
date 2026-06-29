8e805c9d28306f6e481635261e916f3cc29eba53
elithmar <140345599+elithmar@users.noreply.github.com>
2026-06-29 16:39:14 +0700

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="impact-site-verification" content="25d4e176-b6c3-45ad-82ed-1f04cb641400" />
+      </head>
       <body>{children}</body>
     </html>
   );
