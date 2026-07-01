c75e7d26f162fcdf32dbab34d2acc3cfaafdb85d
elithmar <140345599+elithmar@users.noreply.github.com>
2026-07-01 07:52:36 +0700

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
+export const revalidate = 0;
+
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 const supabase = createClient(supabaseUrl, supabaseKey);
-
-export const revalidate = 60;
 
 export default async function Home() {
   const { data: dbTools } = await supabase
